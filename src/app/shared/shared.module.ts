2a33ed19de0e0a7807edb1b882090ec53b9a0bde
FabianAndrade14 <fabianbros_24@gmail.com>
2021-07-17 21:24:08 -0500

    fix(body): Agregada la componente body

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, CarouselComponent],
   imports: [
     CommonModule
   ]
